26de1f41ad294fae40b4cd3ecc93c3960b3546aa
Diego Plentz <diego@plentz.org>
2013-02-27 11:59:06 -0300

    that's what happen when you copy jquery examples :)

--- jquery.maskMoney.js
+++ jquery.maskMoney.js
@@ -352,7 +352,7 @@
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+			$.error( 'Method ' +  method + ' does not exist on jQuery.maskMoney' );
 		}
 	};
 })(jQuery);
