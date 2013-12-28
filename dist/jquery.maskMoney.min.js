bedbebb86c3f023ee827a94a6014727123aa6061
Diego Plentz <diego@plentz.org>
2013-12-28 21:42:49 -0200

    last test of our new build

--- dist/jquery.maskMoney.min.js
+++ dist/jquery.maskMoney.min.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-maskMoney - v2.9.4
+ *  jquery-maskMoney - v2.9.5
  *  jQuery plugin to mask data entry in the input text in the form of money (currency)
  *  https://github.com/plentz/jquery-maskmoney
  *
