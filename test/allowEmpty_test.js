5b69e9a0db14c2dd9493cd92701182757ce5fc39
jpSimkins <jeremy@xstreamides.com>
2014-01-17 23:36:49 -0500

    Added tests for the new setting allowEmpty
    Added test cases for typical use

---
+++ test/allowEmpty_test.js
@@ -0,0 +1,66 @@
+"use strict";
+
+module("allowEmpty");
+
+// Test allowZero: false with 0 default value
+test("allowZero: false -  mask empties 0 value", function() {
+    var input = $("#input4").maskMoney();
+    input.trigger('mask');
+    equal(input.val(), "", "0 value should be empty due to settings");
+});
+
+// Test allowZero: true with 0 default value
+test("allowZero: true - mask keeps 0 value", function() {
+    var input = $("#input5").maskMoney();
+    input.trigger('mask');
+    equal(input.val(), "0.00$", "0 value should be 0.00$ due to settings");
+});
+
+// Test allowZero: false with input changes to a zero value
+test("allowZero: false - input changes to a zero value (empty due to setting)", function() {
+    var input = $("#input4").maskMoney();
+    input.trigger('mask'); // formats the 0 default value
+
+    keypress(input, 1);
+    keypress(input, 2);
+    keypress(input, 3);
+
+    keydown(input, 'backspace');
+    keydown(input, 'backspace');
+    keydown(input, 'backspace');
+
+    equal(input.val(), "", "field should be empty");
+});
+
+// Test allowZero: true with input changes to a zero value
+test("allowZero: true - input changes to a zero value", function() {
+    var input = $("#input5").maskMoney();
+    input.trigger('mask'); // formats the 0 default value
+
+    keypress(input, 1);
+    keypress(input, 2);
+    keypress(input, 3);
+
+    keydown(input, 'backspace');
+    keydown(input, 'backspace');
+    keydown(input, 'backspace');
+
+    equal(input.val(), "0.00$", "field should be 0.00$");
+});
+
+// Test allowZero: true with input changes to an empty value
+test("allowZero: true - input changes to empty", function() {
+    var input = $("#input5").maskMoney();
+    input.trigger('mask'); // formats the 0 default value
+
+    keypress(input, 1);
+    keypress(input, 2);
+    keypress(input, 3);
+
+    keydown(input, 'backspace');
+    keydown(input, 'backspace');
+    keydown(input, 'backspace'); // zero value
+    keydown(input, 'backspace'); // one more backspace will make it empty
+
+    equal(input.val(), "", "field should be empty");
+});
