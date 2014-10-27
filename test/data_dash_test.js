f310be3b2ccf5b7d080cf2298e73359818b9d29f
sgyyz <yangyongzhi1989@gmail.com>
2014-10-27 16:11:19 +0800

    fix the test case

--- test/data_dash_test.js
+++ test/data_dash_test.js
@@ -3,7 +3,7 @@
 module("data-dash api");
 test("with field configured using data-* attributes", function() {
     var input = $("#input3").maskMoney();
-    input.val("12345678");
+    input.val("123.45678");
     input.trigger("focus");
     equal(input.val(), "R$123.456,78", "configure maskMoney using data-* attributes");
 });
