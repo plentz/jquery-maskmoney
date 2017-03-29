6895c667f471a66c93c2593f17c8ec65472f7a4b
Andrew Duncan <abduncan0@gmail.com>
2017-03-29 17:08:04 -0400

    Add comment

--- dist/jquery.maskMoney.js
+++ dist/jquery.maskMoney.js
@@ -263,6 +263,8 @@
                 function isAllTextSelected() {
                     var length = $input.val().length;
                     var selection = getInputSelection();
+                    // This should if all text is selected or if the
+                    // input is empty.
                     return selection.start === 0 && selection.end === length;
                 }
 
